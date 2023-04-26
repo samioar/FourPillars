0e76bbe3bf076e771ecd6cfbb50186e885a1ba52
Samiur Rahman <samiurr2000@gmail.com>
2023-04-26 10:47:32 +0100

    initial commit for safariApp

---
+++ SafariParkApp/SafariParkTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using SafariParkApp;
+using NUnit.Framework;
+namespace SafariParkTest
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
